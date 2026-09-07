8f97c4df109fc262dcc8f3b517419245fb328c18
Tien-Hong Lo <teinhonglo@gmail.com>
2026-09-07 17:52:26 +0800

    Use aligned preprocessing cache in stage 3 evaluation

--- evaluation_example.py
+++ evaluation_example.py
@@ -212,10 +212,7 @@ if __name__ == "__main__":
     #dataset[split_name] = dataset[split_name].map(add_stress_pattern, num_proc=4)
     #metrics, error_cases = calculate_metrics_on_dataset(dataset=dataset[split_name], whistress_client=whistress_client)
     #metrics_wot, error_cases_wot = calculate_metrics_on_dataset(dataset=dataset[split_name], whistress_client=whistress_client, with_transcription=False)
-    is_paired_model = model.__class__.__name__ == "WhiStressPhnPairedResidual"
-    processed_split = (
-        f"{args.split}_paired_v2" if is_paired_model else args.split
-    )
+    processed_split = f"{args.split}_aligned_v2"
     processed_dir = args.data_root / "processed" / args.corpus / processed_split
     dataset = StressDataset(
         hf_dataset_or_path=raw_dataset,
